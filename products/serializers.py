b87102c60e31a97fa8dfa5baaa1798694fd7f706
Md Hasanuzzaman <dr.has82@gmail.com>
2025-08-15 03:35:31 +0600

    Merge done

--- products/serializers.py
+++ products/serializers.py
@@ -49,31 +49,3 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_stock_status(self, obj):
         return obj.stock_status()
-
-from .models import Category, Product
-from rest_framework import serializers
-
-class CategorySerializer(serializers.ModelSerializer):
-    
-    slug = serializers.SlugField(read_only=True)
-    
-    class Meta:
-        model = Category
-        fields = '__all__'
-        
-        
-class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
-
-    class Meta:
-        model = Product
-        fields = '__all__'
-        extra_kwargs = {
-            'slug': {'read_only': True}
-        }
-    
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['discount_percentage'] = instance.discount_percentage()
-        representation['stock_status'] = instance.stock_status()
-        return representation
