2ff5b2cdd0d36859ed045073fbfd3ade01727b96
Md Hasanuzzaman <dr.has82@gmail.com>
2025-08-20 01:17:50 +0600

    Model updatd

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, CustomUser
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import Group
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    model = CustomUser
+
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active")}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 
 @admin.register(Profile)
